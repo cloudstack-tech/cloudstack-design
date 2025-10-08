e5abf6b1ae0a163194fd0f3ae68a1eb2d142c494
rokukoo <1873518094@qq.com>
2025-10-08 19:20:31 +0800

    feat: enhance theme colors and update VSCode settings for Tailwind CSS

---
+++ packages/core/theme/src/colors/base.ts
@@ -0,0 +1,26 @@
+const theme = {
+  default: {
+    DEFAULT: "",
+  },
+  primary: {
+    DEFAULT: "",
+  },
+  secondary: {
+    DEFAULT: "",
+  },
+  success: {
+    DEFAULT: "",
+  },
+  warning: {
+    DEFAULT: "",
+  },
+  danger: {
+    DEFAULT: "",
+  },
+  info: {
+    DEFAULT: "",
+  },
+  light: {
+    DEFAULT: "",
+  },
+};
